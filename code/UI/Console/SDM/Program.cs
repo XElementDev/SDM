320482dda65ba4427f80ce5124ccd3db6673d6eb
IanStorm <IanStorm28@outlook.com>
2017-04-26 23:00:00 +0200

    * added console project in order to being able to manually test the logic

---
+++ code/UI/Console/SDM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using XElement.DotNet.System.Environment.Startup;
+
+namespace XElement.SDM.UI.WinConsole
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var globalFsRetriever = new GlobalFileSystemRetriever();
+            var globalFsResults = globalFsRetriever.Retrieve();
+            OutputResults( globalFsResults );
+
+            var localFsRetriever = new LocalFileSystemRetriever();
+            var localFsResults = localFsRetriever.Retrieve();
+            OutputResults( localFsResults );
+
+            Console.Read();
+        }
+
+
+        private static void OutputResult( IProgramInfo programInfo )
+        {
+            var output = $"ProgramInfo {{ argument = {programInfo.Argument}, " + 
+                         $"filePath = {programInfo.FilePath}, " + 
+                         $"origin = Origin {{ location = {programInfo.Origin.Location} }} }}";
+            Console.WriteLine( output );
+        }
+
+
+        private static void OutputResults( IEnumerable<IProgramInfo> programInfos )
+        {
+            foreach ( var programInfo in programInfos )
+            {
+                OutputResult( programInfo );
+            }
+        }
+    }
+}
