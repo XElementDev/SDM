966922cf4c9a2505e8a5bc238e6ca94cd2f84aed
IanStorm <IanStorm28@outlook.com>
2017-04-29 14:39:31 +0200

    * changed namespace of StartupLogic.Implementation project

--- code/Startup/Startup.Implementation/Properties/AssemblyInfo.cs
+++ code/Startup/Startup.Implementation/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "XElement.SDM.Startup.Implementation" )]
+[assembly: AssemblyTitle( "XElement.SDM.StartupLogic.Implementation" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
