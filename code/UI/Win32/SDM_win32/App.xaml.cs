27138c4ddcf93b0cbda045fe6d24923aecc7d988
IanStorm <IanStorm28@outlook.com>
2017-05-01 17:29:21 +0200

    * application is using Main module now +
    * enabled MEF

--- code/UI/Win32/SDM_win32/App.xaml.cs
+++ code/UI/Win32/SDM_win32/App.xaml.cs
@@ -1,13 +1,47 @@
-﻿using System.Windows;
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.IO;
+using System.Windows;
 
 namespace XElement.SDM.UI.Win32
 {
 #region not unit-tested
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this._mainVM = null;
+
+            this.InitializeMef();
+
+            this.MainWindow = new MainWindow { DataContext = this._mainVM };
+            this.MainWindow.Show();
+        }
+
+
+        private ComposablePartCatalog CreateCatalog()
+        {
+            var catalog = new AggregateCatalog();
+            var assembly = typeof( App ).Assembly;
+            var path = Path.GetDirectoryName( assembly.Location );
+            catalog.Catalogs.Add( new AggregateCatalog( new AssemblyCatalog( assembly ),
+                                                        new DirectoryCatalog( path ) ) );
+            return catalog;
+        }
+
+
+        private void InitializeMef()
+        {
+            var catalog = this.CreateCatalog();
+            var container = new CompositionContainer( catalog );
+
+            container.ComposeParts( this );
+        }
+
+
+        [Import]
+        private Modules.Main.ViewModel _mainVM;
     }
 #endregion
 }
