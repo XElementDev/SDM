b6ab56b1c38a1764484715e5adfaafc7e35c80e1
IanStorm <IanStorm28@outlook.com>
2017-05-01 17:34:12 +0200

    * added some TODOs for the general StartupInfo implementation

---
+++ code/XElement/DotNet/System/Environment/StartupInfo.Implementation/Local32bitRegistryRetriever.cs
@@ -0,0 +1,9 @@
+﻿namespace XElement.DotNet.System.Environment.Startup
+{
+#region not uni-tested
+    public class Local32bitRegistryRetriever
+    {
+        // TODO: use HKEY_CURRENT_USER
+    }
+#endregion
+}
---
+++ code/XElement/DotNet/System/Environment/StartupInfo.Implementation/Local64bitRegistryRetriever.cs
@@ -0,0 +1,9 @@
+﻿namespace XElement.DotNet.System.Environment.Startup
+{
+#region not unit-tested
+    public class Local64bitRegistryRetriever
+    {
+        // TODO: use HKEY_CURRENT_USER
+    }
+#endregion
+}
