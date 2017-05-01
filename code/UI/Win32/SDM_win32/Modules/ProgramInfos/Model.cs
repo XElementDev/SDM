876dd1f1b66d7ccf5bf301b28d6775c729e53f92
IanStorm <IanStorm28@outlook.com>
2017-05-01 17:24:06 +0200

    * added initial (and temporary) version of ProgramInfos module

---
+++ code/UI/Win32/SDM_win32/Modules/ProgramInfos/Model.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using XElement.DotNet.System.Environment.Startup;
+
+namespace XElement.SDM.UI.Win32.Modules.ProgramInfos
+{
+#region not unit-tested
+    [Export]
+    internal class Model : IPartImportsSatisfiedNotification
+    {
+        [ImportingConstructor]
+        public Model()
+        {
+            this._startupInfo = null;
+        }
+
+
+        void IPartImportsSatisfiedNotification.OnImportsSatisfied()
+        {
+            var programInfos = this._startupInfo.Retrieve();
+            this.ProgramInfos = programInfos;
+        }
+
+
+        public IEnumerable<IProgramInfo> ProgramInfos { get; private set; }
+
+
+        [Import]
+        private IStartupInfo _startupInfo;
+    }
+#endregion
+}
---
+++ code/UI/Win32/SDM_win32/Modules/ProgramInfos/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.SDM.UI.Win32.Modules.ProgramInfos
+{
+#region not unit-tested
+    [Export]
+    internal class ViewModel
+    {
+        [ImportingConstructor]
+        public ViewModel( Model model )
+        {
+            this.Model = model;
+        }
+
+
+        public Model Model { get; private set; }
+    }
+#endregion
+}
