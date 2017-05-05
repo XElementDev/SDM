573563e3bcf0ea77d57da98b68fedd0cf2c14163
IanStorm <IanStorm28@outlook.com>
2017-05-05 16:29:40 +0200

    * application icon visible on UI now

--- code/UI/Win32/SDM_win32/Modules/ProgramInfo/ViewModel.cs
+++ code/UI/Win32/SDM_win32/Modules/ProgramInfo/ViewModel.cs
@@ -1,4 +1,11 @@
-﻿namespace XElement.SDM.UI.Win32.Modules.ProgramInfo
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace XElement.SDM.UI.Win32.Modules.ProgramInfo
 {
     // TODO
 #region not unit-tested
@@ -7,13 +14,35 @@
         public ViewModel( Model model )
         {
             this.Model = model;
+            this.InitializeProperties();
         }
 
 
-        //public Icon ApplicationIcon { get; private set; }
+        public ImageSource ApplicationIcon { get; private set; }
 
 
-        public string ApplicationName { get; private set; }
+        //public string ApplicationName { get; private set; }
+
+
+        //  --> https://stackoverflow.com/questions/1127647/convert-system-drawing-icon-to-system-media-imagesource
+        private void InitializeApplicationIcon()
+        {
+            var filePath = this.Model.ProgramInfo.FilePath;
+            var icon = Icon.ExtractAssociatedIcon( filePath );
+            var bmp = icon.ToBitmap();
+            IntPtr hBitmap = bmp.GetHbitmap();
+            var sizeOptions = BitmapSizeOptions.FromEmptyOptions();
+            ImageSource wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap( hBitmap, IntPtr.Zero,
+                                                                           Int32Rect.Empty,
+                                                                           sizeOptions );
+            this.ApplicationIcon = wpfBitmap;
+        }
+
+
+        private void InitializeProperties()
+        {
+            this.InitializeApplicationIcon();
+        }
 
 
         public Model Model { get; private set; }
