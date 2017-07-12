5715f2b05d330727c1584acee29d2ad39da84e12
IanStorm <IanStorm28@outlook.com>
2017-07-12 18:14:49 +0200

    * changed "interesting links" into a TODO such that it is tracked by the IDE

--- code/XElement/DotNet/System/Environment/StartupInfo.Interface/IStartupInfo.cs
+++ code/XElement/DotNet/System/Environment/StartupInfo.Interface/IStartupInfo.cs
@@ -3,7 +3,7 @@
 namespace XElement.DotNet.System.Environment.Startup
 {
     /*
-     * interesting links:
+     * TODO: interesting links:
      * https://blogs.msdn.microsoft.com/windowsstore/2012/06/08/listing-your-desktop-app-in-the-store/
      * https://xamltips.wordpress.com/2015/11/13/brokered-component-for-uwp-on-windows-10/
      * https://stackoverflow.com/questions/34694059/read-write-registery-key-file-in-uwp
