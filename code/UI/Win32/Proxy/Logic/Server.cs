51d8f266ab3dee543eac6e5db10235c53051ccd8
IanStorm <IanStorm28@outlook.com>
2017-08-22 15:29:58 +0200

    * implemented (further) parts of the management proxy logic

---
+++ code/UI/Win32/Proxy/Logic/Server.cs
@@ -0,0 +1,119 @@
+using NamedPipeWrapper;
+using System;
+using System.IO.Pipes;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using XElement.DesignPatterns.CreationalPatterns.FactoryMethod;
+using XElement.DotNet.System.Environment.Startup;
+using XElement.SDM.ManagementLogic;
+
+namespace XElement.SDM.UI.Win32.Proxy.Logic
+{
+#region not unit-tested
+    public class Server
+    {
+        public Server( string pipeName )
+        {
+            this._programLogicFactory = new ProgramLogicFactory();
+            this._pipeName = pipeName; InitializeServerPipe();
+        }
+
+
+        private void ClientConnected( NamedPipeConnection<string, string> connection )
+        {
+            Logger.Get().Log( "A client connected to the server." );
+        }
+
+
+        private void ClientDisconnected( NamedPipeConnection<string, string> connection )
+        {
+            Logger.Get().Log( "A client disconnected from the server." );
+        }
+
+
+        private void ClientMessage( NamedPipeConnection<string, string> connection, string message )
+        {
+            var logMessage = $"Received the following message from client: {message}";
+            Logger.Get().Log( logMessage ); this.DoWork( message );
+            this._serverPipe.PushMessage( string.Empty );
+        }
+
+
+        private static PipeSecurity CreatePipeSecurity()
+        {
+            //  --> 2016-08-11, Ian:
+            //      based on: https://stackoverflow.com/questions/13174660/namedpipeclientstream-can-not-access-to-namedpipeserverstream-under-session-0
+            var pipeSecurity = new PipeSecurity();
+            var sid = new SecurityIdentifier( WellKnownSidType.WorldSid, null );
+            var accessRule = new PipeAccessRule( sid, 
+                                                 PipeAccessRights.ReadWrite, 
+                                                 AccessControlType.Allow );
+            pipeSecurity.AddAccessRule( accessRule );
+            return pipeSecurity;
+        }
+
+
+        private void DoWork( string message )
+        {
+            var programInfo = new MessageParser().Parse( message );
+            if (programInfo == null)
+            {
+                var error = String.Format( "Parsed message (of type {0}) must not be null.", 
+                                           typeof( IProgramInfo ).ToString() );
+                throw new InvalidOperationException( error );
+            }
+            else
+            {
+                var programInfoAsString = Logger.Get().GetLogRepresentationOf( programInfo );
+                Logger.Get().Log( $"Parsed message looks like this: {programInfoAsString}" );
+
+                var command = this._programLogicFactory.Get( programInfo );
+                command.Do();//TODO: Add Undo()!!!
+            }
+        }
+
+
+        private void InitializeServerPipe()
+        {
+            PipeSecurity pipeSecurity = Server.CreatePipeSecurity();
+            this._serverPipe = new NamedPipeServer<string>( this._pipeName, pipeSecurity );
+            this.SubscribeEvents();
+        }
+
+
+        private void OnError( Exception exception )
+        {
+            throw exception;
+        }
+
+
+        public void Start()
+        {
+            this._serverPipe.Start();
+            Logger.Get().Log( "Server started." );
+        }
+
+
+        public void StayAlive()
+        {
+            while (true)
+            {
+            }
+        }
+
+
+        private void SubscribeEvents()
+        {
+            this._serverPipe.ClientConnected += this.ClientConnected;
+            this._serverPipe.ClientDisconnected += this.ClientDisconnected;
+            this._serverPipe.ClientMessage += this.ClientMessage;
+            this._serverPipe.Error += this.OnError;
+        }
+
+
+        private string _pipeName;
+        private IFactory<IProgramLogic, IProgramInfo> _programLogicFactory;
+        private NamedPipeServer<string> _serverPipe;
+    }
+#endregion
+}
