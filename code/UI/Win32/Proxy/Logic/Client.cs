8d2eeca4e0c1a813d479c3a4dd4fbc81346bd223
IanStorm <IanStorm28@outlook.com>
2017-08-22 12:54:59 +0200

    * implemented parts of the management proxy logic

---
+++ code/UI/Win32/Proxy/Logic/Client.cs
@@ -0,0 +1,81 @@
+﻿using NamedPipeWrapper;
+using System;
+
+namespace XElement.SDM.UI.Win32.Proxy.Logic
+{
+#region not unit-tested
+    public class Client
+    {
+        public Client( string pipeName )
+        {
+            this._waitForServerMessage = false;
+            this.InitializeClientPipe( pipeName );
+        }
+
+
+        private void InitializeClientPipe( string pipeName )
+        {
+            this._client = new NamedPipeClient<string>( pipeName );
+            this._client.AutoReconnect = false;
+            this._client.Error += this.OnError;
+            this._client.ServerMessage += this.OnServerMessage;
+        }
+
+
+        private void OnError( Exception exception )
+        {
+            throw exception;
+        }
+
+
+        private void OnServerMessage( NamedPipeConnection<string, string> connection, 
+                                      string message )
+        {
+            this._waitForServerMessage = false;
+        }
+
+
+        public void PlayBack( string message )
+        {
+            this.Start();
+            this.PushMessage( message );
+            this.Stop();
+        }
+
+
+        private void PushMessage( string message )
+        {
+            this._client.PushMessage( message );
+            this.WaitForEndOfTransmission();
+        }
+
+
+        private void Start()
+        {
+            this._client.Start();
+            this._client.WaitForConnection();
+        }
+
+
+        private void Stop()
+        {
+            this._client.Stop();
+            this._client.WaitForDisconnection();
+        }
+
+
+        private void WaitForEndOfTransmission()
+        {
+            this._waitForServerMessage = true;
+            while ( this._waitForServerMessage )
+            {
+            }
+        }
+
+
+        private NamedPipeClient<string> _client;
+
+        private bool _waitForServerMessage;
+    }
+#endregion
+}
---
+++ code/UI/Win32/Proxy/Logic/Logger.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+using XElement.DotNet.System.Environment.Startup;
+
+namespace XElement.SDM.UI.Win32.Proxy.Logic
+{
+#region not unit-tested
+    internal class Logger
+    {
+        private Logger()
+        {
+            this.InitializeEventLogger();
+            this._serializer = new JavaScriptSerializer();
+        }
+
+
+        public static Logger Get()
+        {
+            if ( Logger._singleton == null )
+            {
+                Logger._singleton = new Logger();
+            }
+            return Logger._singleton;
+        }
+
+
+        public string GetLogRepresentationOf( IProgramInfo programInfo )
+        {
+            var stringRepresentation = this._serializer.Serialize( programInfo );
+            return stringRepresentation;
+        }
+
+
+        private void InitializeEventLogger()
+        {
+            if ( !EventLog.SourceExists( Logger.EVENT_SOURCE ) )
+            {
+                EventLog.CreateEventSource( Logger.EVENT_SOURCE, Logger.LOG_NAME );
+            }
+
+            this._eventLog = new EventLog();
+            this._eventLog.Source = Logger.EVENT_SOURCE;
+            this._eventLog.Log = Logger.LOG_NAME;
+        }
+
+
+        public void Log( string logMessage )
+        {
+            this.LogWithTimestamp( logMessage );
+        }
+
+
+        private void LogWithTimestamp( string logMessage )
+        {
+            var dateTime = DateTime.Now.ToString( "yyyy-MM-dd HH:mm:ss.ffff" );
+            var eventLogMessage = $"{dateTime}  Server  {logMessage}";
+
+            this._eventLog.WriteEntry( eventLogMessage, EventLogEntryType.Information );
+            Console.WriteLine( eventLogMessage );
+        }
+
+
+        private const string EVENT_SOURCE = "Management_Proxy";
+        private const string LOG_NAME = "SDM";
+
+
+        private EventLog _eventLog;
+        private JavaScriptSerializer _serializer;
+        private static Logger _singleton;
+    }
+#endregion
+}
