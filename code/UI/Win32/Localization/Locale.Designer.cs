f5b0366d79907a04b6916e9e3bfa66b781560983
IanStorm <IanStorm28@outlook.com>
2017-09-04 20:23:32 +0200

    * added headers describing what is the left part and the right part of the ManagementUC

--- code/UI/Win32/Localization/Locale.Designer.cs
+++ code/UI/Win32/Localization/Locale.Designer.cs
@@ -19,7 +19,7 @@ namespace XElement.SDM.UI.Win32.Localization {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Locale {
@@ -57,6 +57,24 @@ namespace XElement.SDM.UI.Win32.Localization {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Managed by operating system.
+        /// </summary>
+        public static string ManagementUC_OsLabel_Content {
+            get {
+                return ResourceManager.GetString("ManagementUC_OsLabel_Content", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Managed by this application.
+        /// </summary>
+        public static string ManagementUC_SdmLabel_Content {
+            get {
+                return ResourceManager.GetString("ManagementUC_SdmLabel_Content", resourceCulture);
             }
         }
         
