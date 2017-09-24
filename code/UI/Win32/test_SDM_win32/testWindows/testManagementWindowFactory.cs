80fdf4bed6bb1f2c583bacb2adc8378c3d78fdaf
IanStorm <IanStorm28@outlook.com>
2017-09-24 17:32:08 +0200

    * changed file encoding from "UTF-8 with BOM" to "UTF-8", i.e. removed BOM

--- code/UI/Win32/test_SDM_win32/testWindows/testManagementWindowFactory.cs
+++ code/UI/Win32/test_SDM_win32/testWindows/testManagementWindowFactory.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.ComponentModel.Composition;
 using XElement.DesignPatterns.CreationalPatterns.FactoryMethod;
 
