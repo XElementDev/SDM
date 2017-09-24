16d87a2ccb72d6e6f339258389523b1b9ec88dc2
IanStorm <IanStorm28@outlook.com>
2017-09-24 17:06:46 +0200

    * about module/window: implemented window

---
+++ code/UI/Win32/SDM_win32/Modules/About/Window.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace XElement.SDM.UI.Win32
+{
+#region not unit-tested
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+    }
+#endregion
+}
